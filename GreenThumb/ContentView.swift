98ecbc8ae79ea8124cf6e4f24b14e6b9ed901f10
Saulo Nascimento <saulo.nascimento@192.168.0.100>
2025-02-23 09:25:14 -0300

    ïnit project

---
+++ GreenThumb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GreenThumb
+//
+//  Created by Saulo Nascimento on 23/02/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GreenThumb/GreenThumbApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenThumbApp.swift
+//  GreenThumb
+//
+//  Created by Saulo Nascimento on 23/02/25.
+//
+
+import SwiftUI
+
+@main
+struct GreenThumbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
